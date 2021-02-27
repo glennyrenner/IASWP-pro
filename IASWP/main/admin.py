72ec2cd7150e163e9469a119387291b66415fb55
glennyrenner <45600927+glennyrenner@users.noreply.github.com>
2021-02-27 18:09:07 +0100

    Add files via upload

---
+++ IASWP/main/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from main.models import *
+# Register your models here.
+admin.site.register(Location)
+admin.site.register(TargetOperation)
+admin.site.register(HazardEffect)
+admin.site.register(Control)
+admin.site.register(PPE)
+admin.site.register(Hazard)
